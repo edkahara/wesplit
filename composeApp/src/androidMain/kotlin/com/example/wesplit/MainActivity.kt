663272d0c276b00f04d4c58eb0c6fac83df8c9e6
edkahara <edward@ganzdigital.com>
2026-05-02 21:37:07 +0300

    android:for loop

--- composeApp/src/androidMain/kotlin/com/example/wesplit/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/example/wesplit/MainActivity.kt
@@ -6,29 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun AndroidApp() {
-    var name by remember {mutableStateOf(TextFieldValue("world"))}
-
     MaterialTheme {
-        Column(
+        LazyColumn(
             modifier = Modifier
                 .background(Color.White)
                 .safeContentPadding()
@@ -36,13 +28,11 @@ fun AndroidApp() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            TextField(
-                value = name,
-                onValueChange = { newName ->
-                    name = newName
+            for (i in 0 until 100) {
+                item {
+                    Text(text = "Row ${i}!", color = Color.Magenta)
                 }
-            )
-            Text(text = "Hello ${name.text}!", color = Color.Magenta)
+            }
         }
 
     }
